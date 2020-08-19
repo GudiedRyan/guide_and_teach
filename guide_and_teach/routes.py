4973ffc68242e11b3a4d40c49858726cbed5b3d5
Ryan Guide <ryan.guide@comcast.net>
2020-08-19 13:52:32 -0400

    Added navbar and some more features

--- guide_and_teach/routes.py
+++ guide_and_teach/routes.py
@@ -4,10 +4,13 @@ from guide_and_teach.forms import RegistrationForm, LoginForm
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('about.html', title='About Page')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash("Success!")
     return render_template('register.html', title='register', form=form)
