772f7850559de1fa8529c0ec309d8ffb906df594
Ryan Guide <ryan.guide@comcast.net>
2020-08-21 14:45:22 -0400

    Database now works!

--- guide_and_teach/routes.py
+++ guide_and_teach/routes.py
@@ -1,5 +1,6 @@
+import secrets
 from flask import render_template, url_for, flash, redirect
-from guide_and_teach import app, db
+from guide_and_teach import app, db, bcrypt
 from guide_and_teach.forms import RegistrationForm, LoginForm
 from guide_and_teach.models import User, Course
 
@@ -12,7 +13,12 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash("Success!")
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created! You can now log in.')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
