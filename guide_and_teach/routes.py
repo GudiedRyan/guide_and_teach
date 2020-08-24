a0a96789f0d1e400a1f29439f10056b9b41c355e
Ryan Guide <ryan.guide@comcast.net>
2020-08-24 14:44:48 -0400

    login process working

--- guide_and_teach/routes.py
+++ guide_and_teach/routes.py
@@ -1,8 +1,9 @@
 import secrets
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from guide_and_teach import app, db, bcrypt
 from guide_and_teach.forms import RegistrationForm, LoginForm
 from guide_and_teach.models import User, Course
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -11,6 +12,8 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -23,7 +26,20 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page_) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login failed. Check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
