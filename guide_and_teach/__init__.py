3d1015500d1eb77dd1a37631c29c32f92a9da20f
Ryan Guide <ryan.guide@comcast.net>
2020-08-28 13:54:24 -0400

    added some styling

--- guide_and_teach/__init__.py
+++ guide_and_teach/__init__.py
@@ -16,5 +16,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.static_folder = 'static'
 
 from guide_and_teach import routes
