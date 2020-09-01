96ab6465280e206ada77ee3a87cade77f47ed5e7
Ryan Guide <ryan.guide@comcast.net>
2020-09-01 15:41:12 -0400

    Preparing to configure student addition to database

--- guide_and_teach/models.py
+++ guide_and_teach/models.py
@@ -11,6 +11,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     course = db.relationship('Course', backref='user', lazy=True)
+    # student = db.relationship('Student', backref='teacher', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -20,16 +21,33 @@ class Course(db.Model):
     course_title = db.Column(db.String(100), nullable=False)
     course_desc = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    student = db.relationship('Student', backref='class', lazy=True)
+    # student = db.relationship('Student', backref='course', lazy=True)
 
     def __repr__(self):
         return f"Course('{self.course_title}')"
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    student_name = db.Column(db.String(50), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+# class Student(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     student_name = db.Column(db.String(50), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # courselist = db.relationship('CourseList', backref='student', lazy=True)
 
+    # def __repr__(self):
+    #     return f"Student('{self.student_name}')"
+
+# class CourseList(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     student = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+
+
+
+# Page where you add students to DB, where they reference back to User.
+# Then when you go add students, you can have a list of students that you check boxes next to.
+# If the box corresponding to that student is checked, you add that student to the class.
+
+
+#The ALTERNATIVE WAY:
+#Each Course links to a student model that is essentially the list of students within. This was the original idea.
 
 
 
